c12fb73e5fc95238498434eac6eb1ec53fb5e1df
sse <rahul.bytotech@gmail.com>
2021-03-22 18:58:39 +0530

    ini

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Flutter'),
+      ),
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+    );
+  }
+}
